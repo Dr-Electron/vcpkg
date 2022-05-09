1e8c55d5351690e1ab9fb336cf0aa80d6b652142
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-09 09:10:36 +0000

    [onnxruntime-gpu] Update to 1.11.1

--- ports/onnxruntime-gpu/portfile.cmake
+++ ports/onnxruntime-gpu/portfile.cmake
@@ -1,11 +1,11 @@
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
-set(VERSION 1.5.1)
+set(VERSION 1.11.1)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/microsoft/onnxruntime/releases/download/v1.5.1/onnxruntime-win-x64-gpu-1.5.1.zip"
-    FILENAME "onnxruntime-win-x64-gpu-1.5.1.zip"
-    SHA512 893dbed1196b5c9730744dc5566cd3ad8ec677cbea04dd0572efc9a8b3563d3f1cbf40d0dea3d624d9451dc33272c0ae44d53d6deee6f249fa2520e60718ee52
+    URLS "https://github.com/microsoft/onnxruntime/releases/download/v1.11.1/onnxruntime-win-x64-gpu-1.11.1.zip"
+    FILENAME "onnxruntime-win-x64-gpu-1.11.1.zip"
+    SHA512 df85345f0b471a93e6b1416e59412917322e10c51418f981b4bf1a762e6f6d5d0bfc8d4224389051f72739985ed661b17f23726387acc484af407c44b3a14011
 )
 
 vcpkg_extract_source_archive_ex(
@@ -24,18 +24,18 @@ file(MAKE_DIRECTORY
     )
 
 file(COPY
-        ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.5.1/include
+        ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.11.1/include
         DESTINATION ${CURRENT_PACKAGES_DIR}
     )
 
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.5.1/lib/onnxruntime.lib
+file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.11.1/lib/onnxruntime.lib
     DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.5.1/lib/onnxruntime.lib
+file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.11.1/lib/onnxruntime.lib
     DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.5.1/lib/onnxruntime.dll
+file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.11.1/lib/onnxruntime.dll
     DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.5.1/lib/onnxruntime.dll
+file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.11.1/lib/onnxruntime.dll
     DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
 
 # # Handle copyright
-file(INSTALL ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.5.1/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.11.1/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
